a878dab7e46f56c1f3792f411cdd00e84702ed36
Justforyou Innovation <justforyou.technique@gmail.com>
2026-05-23 22:17:31 +0630

    second update server

--- controllers/productdeleteController.js
+++ controllers/productdeleteController.js
@@ -1,8 +1,32 @@
 import dashboardModel from "../models/dashboardModel.js";
+import supabase from "../config/supabase.js";
 
 const pdController = async(req, res)=>{
     try{
-        await dashboardModel.findByIdAndDelete(req.params.id);
+        const id = req.params.id;
+        //Find product first
+        const product = await dashboardModel.findById(id);
+
+        if(!product){
+            return res.status(404).json({
+                message:"Product not found"
+            });
+        }
+
+        //Delete image from Supabase
+        if(product.file){
+            const fileName = product.file.split("/").pop();
+            const {err} = await supabase.storage
+                .from("productimages")
+                .remove([fileName]);
+
+            if(err){
+                console.log(err)
+            }
+        }
+
+        //Delete product from MongoDB
+        await dashboardModel.findByIdAndDelete(id);
         res.json("Deleted Successful");
     }catch(err){
         res.status(500).json({message:err.message});
